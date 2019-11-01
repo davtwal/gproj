210149a3ec75c89c3aa827b2f9fe9b4f6ef80b92
David Walker <davtwal@gmail.com>
2019-10-31 17:19:28 -0700

    Sexy new teapot and sexy new model loading that accounts for .obj files giving different combinations of position/texture coordinates

--- src/MeshManager.cpp
+++ src/MeshManager.cpp
@@ -18,7 +18,35 @@
 #include "Trace.h"
 #include <algorithm>
 #include <filesystem>
+#include <unordered_set>
 namespace fs = std::filesystem;
+
+bool operator==(tinyobj::index_t const& a, tinyobj::index_t const& b) {
+  return a.vertex_index == b.vertex_index && a.texcoord_index == b.texcoord_index && a.normal_index == b.normal_index;
+}
+
+namespace std {
+  template <>
+  struct equal_to<tinyobj::index_t> { // functor for operator==
+    _CXX17_DEPRECATE_ADAPTOR_TYPEDEFS typedef tinyobj::index_t first_argument_type;
+    _CXX17_DEPRECATE_ADAPTOR_TYPEDEFS typedef tinyobj::index_t second_argument_type;
+    _CXX17_DEPRECATE_ADAPTOR_TYPEDEFS typedef bool             result_type;
+
+    constexpr bool operator()(const tinyobj::index_t& _Left, const tinyobj::index_t& _Right) const {
+      return _Left.vertex_index == _Right.vertex_index && _Left.texcoord_index == _Right.texcoord_index && _Left.
+             normal_index == _Right.normal_index;
+    }
+  };
+
+  template <>
+  struct hash<tinyobj::index_t> {
+    size_t operator()(const tinyobj::index_t& i) const noexcept {
+      return (hash<int>()(i.vertex_index) << 32)
+             ^ (hash<int>()(i.texcoord_index) << 16)
+             ^ (hash<int>()(i.normal_index));
+    }
+  };
+}
 
 namespace dw {
   void MeshManager::clear() {
@@ -76,145 +104,156 @@ namespace dw {
      *    - Also includes maps for rough/metallic/sheen, and emissive maps/normal maps
      *    - And texture options
      */
-    attrib_t attributes;
-    std::vector<shape_t> shapes;
+    attrib_t                attributes;
+    std::vector<shape_t>    shapes;
     std::vector<material_t> materials;
-    std::string warnString;
-    std::string errString;
+    std::string             warnString;
+    std::string             errString;
 
     fs::path mtlPath = fs::current_path() / "data" / "materials";
-    bool worked = LoadObj(&attributes, &shapes, &materials, &warnString, &errString, filename.c_str(), mtlPath.generic_string().c_str());
+    bool     worked  = LoadObj(&attributes,
+                               &shapes,
+                               &materials,
+                               &warnString,
+                               &errString,
+                               filename.c_str(),
+                               mtlPath.generic_string().c_str());
 
     if (!errString.empty())
       Trace::Error << "Model Loading Error: " << errString << Trace::Stop;
     if (!warnString.empty())
       Trace::Warn << "Model Loading Warning: " << warnString << Trace::Stop;
 
-    if(!worked) {
+    if (!worked) {
       return std::numeric_limits<MeshKey>::max();
     }
 
-    bool computeNormals = attributes.normals.empty();
+    bool computeNormals  = attributes.normals.empty();
     bool computeTangents = !attributes.texcoords.empty();
 
-    std::vector<Vertex> vertices;
-    const size_t vertexCount = attributes.vertices.size() / 3;
     assert(attributes.vertices.size() % 3 == 0);
+    const size_t                        vertexCount = attributes.vertices.size() / 3;
+    std::vector<Vertex>                 vertices;
+    std::vector<uint32_t>               indices;
+    std::unordered_map<index_t, size_t> vertexIndexCombos;
 
-    vertices.reserve(vertexCount);
-    for(size_t i = 0; i < attributes.vertices.size(); i += 3) {
-      Vertex v;
+    // Getting a vertex of index i : attributes.vertices[i * 3] attributes.vertices[i * 3 + 1] attributes.vertices[i * 3 + 2]
+    // Vertex info:
+    //  - Position
+    //  - Normal
+    //  - Tangent
+    //  - Bitangent
+    //  - Texcoord
+    //  - Color
+    // A vertex is created for each UNIQUE COMBINATION of data
+    // If a face has 8/2 3/1 4/2 as its vi/ti, and another has 8/1 3/1 4/2, then we'll end up with
+    // four total vertices: 8/1, 8/2, 3/1, 4/2.
+    size_t duplicates_saved = 0;
+    for (auto& shape : shapes) {
+      auto& mesh = shape.mesh;
+      for (uint32_t i = 0; i < mesh.indices.size(); ++i) {
+        auto& index = mesh.indices[i];
 
-      v.pos = glm::vec3(attributes.vertices[i],  attributes.vertices[i + 1],  attributes.vertices[i + 2]);
+        // if this combination is already a part of the data
+        auto val = vertexIndexCombos.find(index);
+        if (val != vertexIndexCombos.end()) {
+          indices.push_back(val->second);
+          ++duplicates_saved;
+        }
+        else {
+          // new unique combination
+          auto& vi = index.vertex_index;
+          auto& ti = index.texcoord_index;
+          auto& ni = index.normal_index;
 
-      if(i < attributes.normals.size())
-        v.normal = glm::vec3(attributes.normals[i],   attributes.normals[i + 1],   attributes.normals[i + 2]);
+          Vertex v;
+          v.pos = {attributes.vertices[vi * 3], attributes.vertices[vi * 3 + 1], attributes.vertices[vi * 3 + 2]};
 
-      if(i < attributes.texcoords.size())
-        v.texCoord = glm::vec2(attributes.texcoords[i], attributes.texcoords[i + 1]);
+          if (!attributes.normals.empty())
+            v.normal = {attributes.normals[ni * 3], attributes.normals[ni * 3 + 1], attributes.normals[ni * 3 + 2]};
 
-      if(i < attributes.colors.size())
-        v.color = glm::vec3(attributes.colors[i],    attributes.colors[i + 1],    attributes.colors[i + 2]);
+          if (!attributes.texcoords.empty())
+            v.texCoord = glm::vec2{attributes.texcoords[ti * 2], attributes.texcoords[ti * 2 + 1]};
 
-      vertices.push_back(v);
+          if (!attributes.colors.empty())
+            v.color = {attributes.colors[vi * 3], attributes.colors[vi * 3 + 1], attributes.colors[vi * 3 + 2]};
+
+          indices.push_back(vertices.size());
+          vertexIndexCombos.try_emplace(index, vertices.size());
+
+          vertices.push_back(v);
+        }
+      }
     }
 
-    size_t faceCount = 0;
-    // do per-face fixing (compute normals, compute tangents)
-    for(auto& shape : shapes) { // += 3 here because we triangulated.
-      for (size_t i = 0; i < shape.mesh.indices.size(); i += 3) {
-        ++faceCount;
+    vertices.shrink_to_fit();
+    indices.shrink_to_fit();
 
-        // http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-13-normal-mapping/
-        auto& index0 = shape.mesh.indices[i];
-        auto& index1 = shape.mesh.indices[i + 1];
-        auto& index2 = shape.mesh.indices[i + 2];
+    Trace::All << "Mesh Loading Duplicates (" << filename << "): " << duplicates_saved << Trace::Stop;
 
-        auto& v0 = vertices[index0.vertex_index];
-        auto& v1 = vertices[index1.vertex_index];
-        auto& v2 = vertices[index2.vertex_index];
-        glm::vec3 deltaP0 = v1.pos - v0.pos;
-        glm::vec3 deltaP1 = v2.pos - v0.pos;
+    // We now have a complete list of VERTICES and INDICES for a complete mesh.
+    // Compute normals / tangents / bitangents
 
-        if (computeNormals) {
-          glm::vec3 N = normalize(cross(deltaP0, deltaP1));
-          v0.normal += N;
-          v1.normal += N;
-          v2.normal += N;
-        }
+    // for each triangle
+    assert(indices.size() % 3 == 0);
+    for (size_t i = 0; i < indices.size(); i += 3) {
+      auto& v0 = vertices[indices[i]];
+      auto& v1 = vertices[indices[i + 1]];
+      auto& v2 = vertices[indices[i + 2]];
 
-        if (computeTangents) {
-          if (index0.texcoord_index == -1 || index1.texcoord_index == -1 || index2.texcoord_index == -1)
-            continue;
+      glm::vec3 deltaP0 = v1.pos - v0.pos;
+      glm::vec3 deltaP1 = v2.pos - v0.pos;
 
-          auto& uv0 = vertices[index0.texcoord_index].texCoord;
-          auto& uv1 = vertices[index1.texcoord_index].texCoord;
-          auto& uv2 = vertices[index2.texcoord_index].texCoord;
+      if (computeNormals) {
+        glm::vec3 N = normalize(cross(deltaP0, deltaP1));
+        v0.normal += N;
+        v1.normal += N;
+        v2.normal += N;
+      }
 
-          glm::vec2 deltaUV0 = uv1 - uv0;
-          glm::vec2 deltaUV1 = uv2 - uv0;
+      if (computeTangents) {
+        glm::vec2 deltaUV0 = v1.texCoord - v0.texCoord;
+        glm::vec2 deltaUV1 = v2.texCoord - v0.texCoord;
 
-          float denom = 1.f / (deltaUV0.x * deltaUV1.y - deltaUV0.y * deltaUV1.x);
+        float denom = 1.f / (deltaUV0.x * deltaUV1.y - deltaUV0.y * deltaUV1.x);
 
-          // normalize?
-          glm::vec3 tangent = (deltaP0 * deltaUV1.y - deltaP1 * deltaUV0.y) * denom;
-          glm::vec3 bitangent = (deltaP1 * deltaUV0.x - deltaP0 * deltaUV1.x) * denom;
+        glm::vec3 tan  = (deltaP0 * deltaUV1.y - deltaP1 * deltaUV0.y) * denom;
+        glm::vec3 btan = (deltaP1 * deltaUV0.x - deltaP0 * deltaUV1.x) * denom;
 
-          vertices[index0.vertex_index].tangent += tangent;
-          vertices[index1.vertex_index].tangent += tangent;
-          vertices[index2.vertex_index].tangent += tangent;
+        v0.tangent += tan;
+        v1.tangent += tan;
+        v2.tangent += tan;
 
-          vertices[index0.vertex_index].bitangent += bitangent;
-          vertices[index1.vertex_index].bitangent += bitangent;
-          vertices[index2.vertex_index].bitangent += bitangent;
-        }
+        v0.bitangent += btan;
+        v1.bitangent += btan;
+        v2.bitangent += btan;
       }
     }
 
     // POST-PROCESS STEPS
     // Normalize normals, tangents, and bitangents
-    for(auto& vert : vertices) {
-      vert.normal = normalize(vert.normal);
-      vert.tangent = normalize(vert.tangent);
+    for (auto& vert : vertices) {
+      vert.normal    = normalize(vert.normal);
+      vert.tangent   = normalize(vert.tangent);
       vert.bitangent = normalize(vert.bitangent);
     }
 
     // Center vertices
     glm::vec3 rollingAverage = vertices[0].pos;
-    for(size_t i = 1; i < vertices.size(); ++i) {
+    for (size_t i = 1; i < vertices.size(); ++i) {
       rollingAverage = (vertices[i].pos + glm::vec3(i) * rollingAverage) / glm::vec3(i + 1);
     }
 
-    float biggestExtent = glm::length2(vertices[0].pos - rollingAverage);
-    for(auto& vert : vertices) {
+    float biggestExtent = length2(vertices[0].pos - rollingAverage);
+    for (auto& vert : vertices) {
       vert.pos -= rollingAverage;
-      biggestExtent = std::max(biggestExtent, glm::length2(vert.pos));
+      biggestExtent = std::max(biggestExtent, length2(vert.pos));
     }
 
     biggestExtent = 1.f / sqrt(biggestExtent);
-    for(auto& vert : vertices) {
+    for (auto& vert : vertices) {
       vert.pos *= biggestExtent;
     }
-
-    // form index vector
-    std::vector<uint32_t> indices;
-    indices.reserve(faceCount * 3); // *3 because triangulated
-
-    for(auto& shape : shapes) {
-      for(size_t i = 0; i < shape.mesh.indices.size(); i += 3) {
-        // This reverses the winding of the faces.
-        indices.push_back(shape.mesh.indices[i].vertex_index);
-        if (flipWinding) {
-          indices.push_back(shape.mesh.indices[i + 2].vertex_index);
-          indices.push_back(shape.mesh.indices[i + 1].vertex_index);
-        } else {
-          indices.push_back(shape.mesh.indices[i + 1].vertex_index);
-          indices.push_back(shape.mesh.indices[i + 2].vertex_index);
-        }
-      }
-    }
-
-    indices.shrink_to_fit();
 
     return addMesh(vertices, indices).first;
   }
